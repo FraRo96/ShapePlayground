b2134fb26b12f1367410c75d5ca1e5eea3dc40cf
Francesco Rossini <francesco.rossini@linksfoundation.com>
2025-05-21 11:05:11 +0200

    v2: Morph animated progress.

--- app/src/main/java/com/example/shapeplayground/Screen.kt
+++ app/src/main/java/com/example/shapeplayground/Screen.kt
@@ -1,41 +1,60 @@
 package com.example.shapeplayground
 
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.asComposePath
-import androidx.compose.ui.graphics.drawscope.scale
-import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.graphics.shapes.Morph
 import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.circle
 import androidx.graphics.shapes.toPath
 import com.example.shapeplayground.ui.theme.ShapePlaygroundTheme
 
 @Composable
 fun Screen(modifier: Modifier = Modifier) {
+
+    val infiniteTransition = rememberInfiniteTransition(label = "morph")
+    val morphProgress by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "morphProgress"
+    )
+
     Box(
         modifier = modifier
             .drawWithCache {
-                val roundedPolygon = RoundedPolygon(
+                val exagon = RoundedPolygon(
                     numVertices = 6,
                     radius = size.minDimension / 2,
                     centerX = size.width / 2,
                     centerY = size.height / 2
                 )
-                val path = roundedPolygon.toComposePath()
+                val circle = RoundedPolygon.circle(
+                    radius = size.minDimension / 2,
+                    centerX = size.width / 2,
+                    centerY = size.height / 2
+                )
+                val path = exagon.toComposePath()
+                val morphPath = Morph(start = exagon, end = circle).toComposePath(progress = morphProgress)
                 onDrawBehind {
-                    drawPath(path = path, color = Color.Cyan.copy(0.9f))
+                    drawPath(path = morphPath, color = Color.Cyan.copy(0.9f))
+                    //drawPath(path = path, color = Color.Red.copy(0.9f))
                 }
             }.fillMaxSize()
     ) {
